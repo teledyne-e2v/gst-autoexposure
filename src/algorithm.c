eaf4635c7a72235d49aeb02d79a8b68e13da0704
lchevallier-teledyne <124684007+lchevallier-teledyne@users.noreply.github.com>
2023-03-17 14:43:55 +0100

    new algorithm with digital gain (not compiled)

--- src/algorithm.c
+++ src/algorithm.c
@@ -66,6 +66,116 @@ void algorithm_without_exposition_v2(float global_mean, int latency, int lowerbo
 	}
 }
 
+void algorithm_without_exposition_v2(float global_mean, int latency, int lowerbound, int upperbound)
+{
+	int gain = get_control("analog_gain");
+	int max_gain = get_control_max("analog_gain");
+	int newGain;
+	int interbound = (upperbound + lowerbound) / 2;
+	int calc = (log10(interbound) + 0.09 * gain - log10(global_mean)) / 0.07;
+	if (proc_once2 == 0)
+	{
+		if (calc < max_gain)
+		{
+			newGain = calc;
+		}
+		else
+		{
+			newGain = max_gain;
+		}
+		set_control("analog_gain", newGain);
+		proc_once2 = 1;
+	}
+	if (proc_once2 > 0)
+	{
+		proc_once2++;
+	}
+
+	if (proc_once2 > latency && (global_mean > upperbound || global_mean < lowerbound))
+	{
+		proc_once2 = 0;
+	}
+}
+
+int algorithm_digital_gain(int target, int global_mean, int digital_gain)
+{
+	int new_digital_gain = digital_gain * (target/(float)global_mean);
+	if(new_digital_gain < 256) //256 is select as min because if the digital gain is < 256 the image will be grayed out
+	{
+		new_digital_gain=256;
+		set_control("digital_gain", 256);
+		return 256 * (global_mean /(float) digital_gain); // expected value of global mean with the gain set to 256
+	}
+	else if(new_digital_gain > get_control_max("digital_gain"))
+	{
+		set_control("digital_gain", get_control_max("digital_gain"));
+	}
+	else
+	{
+		set_control("digital_gain", new_digital_gain);
+	}
+	return 0;
+}
+
+int algorithm_analog_gain(int target, int global_mean, int analog_gain)
+{
+	int new_analog_gain = (log10(target) + 0.09 * analog_gain - log10(global_mean)) / 0.07;
+	if(new_analog_gain < get_control_min("analog_gain"))
+	{
+		new_analog_gain=get_control_min("analog_gain");
+		set_control("analog_gain", new_analog_gain);
+		return pow(10, 0.07 * new_analog_gain - 0.09 * analog_gain + log10(global_mean)); // expected value of global mean with the gain set to 256
+	}
+	else if(new_analog_gain > get_control_max("digital_gain"))
+	{
+		new_analog_gain = get_control_max("digital_gain");
+		set_control("digital_gain", new_analog_gain);
+		return pow(10, 0.07 * new_analog_gain - 0.09 * analog_gain + log10(global_mean));
+	}
+	else
+	{
+		set_control("digital_gain", new_analog_gain);
+	}
+	return 0;
+}
+
+void algorithm_without_exposition_v3(float global_mean, int latency, int target)
+{
+	int analog_gain = get_control("analog_gain");
+	int digital_gain = get_control("digital_gain");
+	if (proc_once2 == 0)
+	{
+		if(target > global_mean)
+		{
+			int expected = algorithm_analog_gain(target,global_mean,analog_gain);
+			if(expected != 0)
+			{
+				algorithm_digital_gain(target,global_mean,digital_gain);
+			}
+		}
+		else
+		{
+			int expected = algorithm_digital_gain(target,global_mean,analog_gain);
+			if(expected != 0)
+			{
+				algorithm_analog_gain(target,global_mean,digital_gain);
+			}
+		}
+		proc_once2 = 1;
+	}
+	if (proc_once2 > 0)
+	{
+		proc_once2++;
+	}
+
+	if (proc_once2 > latency && (global_mean > target + 5 || global_mean < target - 5))
+	{
+		proc_once2 = 0;
+	}
+}
+
+
+
 void algorithm_with_exposition_v3(float global_mean, int maxExp, int latency, int lowerbound, int upperbound)
 {
 	int exp1 = get_control("exposure");
@@ -84,19 +194,19 @@ void algorithm_with_exposition_v3(float global_mean, int maxExp, int latency, in
 		else
 		{
 			set_control("exposure", maxExp);
-			algorithm_without_exposition_v2(delta * global_mean, latency, lowerbound, upperbound);
+			algorithm_without_exposition_v3(delta * global_mean, latency, lowerbound, upperbound);
 		}
 	}
 	else if (global_mean < lowerbound && proc_Once > latency)
 	{
-		algorithm_without_exposition_v2(global_mean, latency, lowerbound, upperbound);
+		algorithm_without_exposition_v3(global_mean, latency, lowerbound, upperbound);
 	}
 	else if (global_mean > upperbound)
 	{
 
 		if (gain > 0)
 		{
-			algorithm_without_exposition_v2(global_mean, latency, lowerbound, upperbound);
+			algorithm_without_exposition_v3(global_mean, latency, lowerbound, upperbound);
 		}
 		else if (proc_Once == 0)
 		{
